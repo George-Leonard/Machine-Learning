edde737bbd87743c528edb3dec2cc6f887f58eb0
George <38766881+George-Leonard@users.noreply.github.com>
2021-01-26 05:36:08 +0000

    Update titanic.py

--- Titanic Problem/code/titanic.py
+++ Titanic Problem/code/titanic.py
@@ -3,25 +3,55 @@ import numpy as np
 from processdb import process
 import pandas as pd
 
-db = pd.read_csv("train.csv")
-db = process.pandasrenameheaders(db, db.loc[0])
+dbtr = pd.read_csv("train.csv")
+dbtr = process.pandasrenameheaders(dbtr, dbtr.loc[0])
 
-db.Sex = db.Sex.replace("female", "1")
-db.Sex = db.Sex.replace("male", "0")
-db = db.dropna()
+dbtr.Sex = dbtr.Sex.replace("female", "1")
+dbtr.Sex = dbtr.Sex.replace("male", "0")
 
-x = db.drop(columns = ["Embarked", "Cabin", "Fare", "Ticket", "Name", "PassengerId","Survived"])
+dbtr = dbtr.dropna()
+
+x = dbtr.drop(columns = ["Embarked", "Cabin", "Fare", "Ticket", "Name", "PassengerId","Survived"])
 x = x.astype(float)
 
-y = db["Survived"]
+xtr = x[0:173]
+xte = x[173:183]
+
+y = dbtr["Survived"]
 y = y.astype(float)
+ytr = y[0:173]
+yte = y[173:183] #tests only against 10 points, wanted to make sure the classifier
+                 #had the most data to work with in order to increase accuracy
 
-print(x)
-print(y)
+xtr.reset_index(drop=True)
+ytr.reset_index(drop=True)
+xte.reset_index(drop=True)
+yte.reset_index(drop=True)
 
-clf = KNeighborsClassifier(n_neighbors=1)
+clfKNN = KNeighborsClassifier(n_neighbors=1)
 
-clf.fit(x,y)
+clfKNN.fit(xtr,ytr)
+
+predsKNN = []
+
+for i in range(0,len(xte)):
+    TMP = xte.iloc[i]
+    TMP = np.array(TMP)
+    TMP = TMP.reshape(1,-1)
+    predsKNN.append(int(clfKNN.predict(TMP)))
+
+correctKNN = 0
+
+for i in range(0,len(predsKNN)):
+    TMP = yte.iloc[i]
+    TMP = np.array(TMP)
+    TMP = TMP.reshape(1,-1)
+    if predsKNN[i] == yte.iloc[i]:
+        correctKNN+=1
+    else:
+        correctKNN+=0
+
+print("KNN accuracy:", correctKNN/len(xte))
 
 val1 = float(input("Ticket class [1st class (1)-3rd class (3)]: "))
 val2 = input("Sex [M/F]: ")
@@ -35,10 +65,9 @@ val5 = float(input("Number of parents/children on board: "))
 
 predict = np.array([[val1,val2,val3,val4,val5]])
 
-pred = clf.predict(predict)
+pred = clfKNN.predict(predict)
 
 if pred == 1:
-    print("Alive")
+    print("prediction: Alive")
 else:
-    print("Dead")
-
+    print("prediction: Dead")
